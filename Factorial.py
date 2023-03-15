0680d4864e6febdd2edd5b61bae30fa7c9f21e9e
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-03-15 22:50:57 +0530

    def factorial

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+def factorial():
+    n=int(input("Enter a number to find factorial:"))
+    s=1
+    for i in range(1,n+1):
+        s=s*i
+        print("factorial=",s)
+
+factorial()
+factorial()
+factorial()
